4e684bd95e4e1b99918992e645866a9835f50e3d
Walnuts <zed.walnuts.king@gmail.com>
2023-12-15 15:00:14 +0800

    Added buyTicket Functions
    The finalize ticket functions are not done yet

---
+++ app/src/main/java/com/example/eventticketing/BuyTicketActivity.kt
@@ -0,0 +1,58 @@
+package com.example.eventticketing
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class BuyTicketActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.buy_tickets)
+
+        val tvTicketNumber = findViewById<TextView>(R.id.tvTickNumber)
+        val tvTicketSubtotal = findViewById<TextView>(R.id.tvTicketSubtotal)
+        val tvTicketNum = findViewById<TextView>(R.id.ticketNum)
+        val pricePerTicket = 1660 // get this from a textview
+
+        // increment - decrement buttons
+        val decrement = findViewById<ImageView>(R.id.ticketDecrement1)
+        val increment = findViewById<ImageView>(R.id.ticketIncrement1)
+
+        decrement.setOnClickListener() {
+            var current = tvTicketNumber.text.toString().toInt()
+            if (current > 1) {
+                tvTicketNumber.text = (current - 1).toString()
+                tvTicketNum.text = (current - 1).toString() + " Tickets"
+                tvTicketSubtotal.text = "PHP " + (current*pricePerTicket).toString()
+            }
+        }
+
+        increment.setOnClickListener() {
+            var current = tvTicketNumber.text.toString().toInt()
+            tvTicketNumber.text = (current + 1).toString()
+            tvTicketNum.text = (current + 1).toString() + " Tickets"
+            tvTicketSubtotal.text = "PHP " + (current*pricePerTicket).toString()
+        }
+
+        // back and next buttons
+        val btnBack = findViewById<ImageView>(R.id.btnBack)
+        btnBack.setOnClickListener() {
+            val userEmail = intent.getStringExtra("email").toString().trim()
+            val intentHome = Intent(this, HomeActivity::class.java)
+            intentHome.putExtra("email", userEmail)
+            startActivity(intentHome)
+        }
+
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        btnNext.setOnClickListener() {
+            val userEmail = intent.getStringExtra("email").toString().trim()
+            val intentFinalizeTicket = Intent(this, FinalizeTicketActivity::class.java)
+            intentFinalizeTicket.putExtra("email", userEmail)
+            intentFinalizeTicket.putExtra("ticketNum", tvTicketNum.text.toString().trim())
+            startActivity(intentFinalizeTicket)
+        }
+    }
+}
--- app/src/main/java/com/example/eventticketing/HomeActivity.kt
+++ app/src/main/java/com/example/eventticketing/HomeActivity.kt
@@ -1,4 +1,32 @@
 package com.example.eventticketing
 
-class HomeActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class HomeActivity :  AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+        val intent = intent
+
+        val btnMyTickets = findViewById<Button>(R.id.btnMyTickets)
+        btnMyTickets.setOnClickListener() {
+            // send to myTickets activity
+            // make sure to send with email for database
+        }
+
+        val btnTickets = findViewById<Button>(R.id.btnTickets)
+        btnTickets.setOnClickListener() {
+            val userEmail = intent.getStringExtra("email").toString().trim()
+            val intentBuyTickets = Intent(this, BuyTicketActivity::class.java)
+            intentBuyTickets.putExtra("email", userEmail)
+            startActivity(intentBuyTickets)
+        }
+
+
+    }
 }
